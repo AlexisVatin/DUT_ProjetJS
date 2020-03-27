a414925d78bb03b3ed2b5f0574b6d7c89aeea44c
ALEXIS <alexis.vatin@etu.univ-amu.fr>
2020-03-27 18:18:14 +0100

    maj

--- php/connexion.php
+++ php/connexion.php
@@ -6,21 +6,21 @@ $obj->success = false;
 $obj->message = 'Nom d\'utilisateur ou mot de passe incorrect';
 
 // PHP a rempli $_POST[username] et $_POST[password]
-$username=$_POST["username"];
-$password=$_POST["password"];
-$sql ="SELECT * FROM user WHERE name = '$username'";
-$result = $bdd->prepare();
-$result->execute();
-foreach ($result as $r){
-    if ($password == $r["password"]) {
-        $_SESSION["username"]=$username;
-    }
-}
-//$found = true; // simule user/pass trouvés en base
-//if ($found) {
-//    $obj->success = true;
-//    $_SESSION['user'] = 123;
+//$username=$_POST["username"];
+//$password=$_POST["password"];
+//$sql ="SELECT * FROM user WHERE name = '$username'";
+//$result = $bdd->prepare();
+//$result->execute();
+//foreach ($result as $r){
+//    if ($password == $r["password"]) {
+//        $_SESSION["username"]=$username;
+//    }
 //}
+$found = true; // simule user/pass trouvés en base
+if ($found) {
+    $obj->success = true;
+    $_SESSION['user'] = 123;
+}
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
